4a96ac4d3abb16856c305fd0827f2bdace21ec63
Vinicius Pinheiro <carraravinicius155@gmail.com>
2023-07-13 22:52:44 -0400

    first commit

---
+++ resources/js/Root.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+
+interface RootProps {
+    
+}
+ 
+interface RootState {
+    
+}
+ 
+class Root extends React.Component<RootProps, RootState> {
+    render() { 
+        return (
+            <section>
+                <h1>Root Page</h1>
+            </section>
+          );
+    }
+}
+ 
+export default Root;
